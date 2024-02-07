79e03e5d0e3aac1a77bb36ce12243df4b936a4fd
yasminie <99419128+yasminie@users.noreply.github.com>
2024-02-07 11:32:41 -0500

    added clear fields

--- html/js/general.js
+++ html/js/general.js
@@ -397,3 +397,10 @@ function validatePhone(phone){
     const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
     return regex.test(phone);
 }
+
+function clearFields(){
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+}
